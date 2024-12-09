7472a7da7f27a1d75fbabbf196214c76e401fd1f
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-12-09 22:31:41 +0530

    Bug fixed on Data Extractions

--- Services/data_Extract.py
+++ Services/data_Extract.py
@@ -1,51 +1,6 @@
 import pdfplumber as pp  # Handle the PDF file
 import re
 
-  # Handle regular expressions
-# import nltk  # Natural Language Toolkit for text processing
-# from nltk.corpus import stopwords  # To remove stopwords
-# from nltk.stem import WordNetLemmatizer
-# # 
-# # Download necessary NLTK data
-# nltk.download("stopwords")  # Required internet connection
-# nltk.download("wordnet")
-
-# # Initialize lemmatizer
-# lemmatizer = WordNetLemmatizer()
-
-# # Read PDF and extract text
-# data = ''
-# with pp.open("Project.pdf") as pdf:
-#     for page in pdf.pages:
-#         data += page.extract_text()
-
-# # Preprocessing: Split into sentences
-# sentences = data.split('.')  # Split on periods to get individual sentences
-
-# # Remove stopwords, special characters, and lemmatize
-# stop_words = set(stopwords.words('english'))  # Use a set for faster lookup
-# filtered_tokens = []
-
-# for sentence in sentences:
-#     # Remove special characters using regex
-#     clean_sentence = re.sub(r'[^a-zA-Z0-9\s]', '', sentence)  # Keep only alphanumeric and spaces
-#     # Tokenize the cleaned sentence into words
-#     tokens = clean_sentence.split()
-#     lemmatized_sen=[]
-#     # Filter out stopwords and lemmatize
-#     for token in tokens:
-#         if token.lower() not in stop_words:  # Convert to lowercase for uniformity
-#             lemmatized_word = lemmatizer.lemmatize(token.lower())  # Lemmatize the token
-#             lemmatized_sen.append(lemmatized_word)
-#     filtered_tokens.append(lemmatized_sen)
-
-# # Print the filtered tokens
-# # print(filtered_tokens)
-
-
-
-
-
 class DataExtractor:
 
     def __init__(self, file_path):
@@ -68,6 +23,8 @@ class DataExtractor:
     def __clean_Paragraph(self,sentence):
         try:
             clean_sentence = re.sub(r'[^a-zA-Z0-9]', ' ',sentence)
+            clean_sentence=re.sub(' +',' ', clean_sentence)
+            clean_sentence=clean_sentence.strip()
             return clean_sentence
         except Exception as e:
             raise RuntimeError(f"An error occures while cleaning the paragraph:{str(e)}")
@@ -82,15 +39,10 @@ class DataExtractor:
             raise RuntimeError(f"An error occured while processing Sentence:{str(e)} ")
 
 
-path="/home/ghost/Documents/My_projects/Business_Projects/Products/Custom-RAG-Lama-ChatBot/Services/Project.pdf"
 
-obj= DataExtractor(path)
+if __name__=="__main__":
+    path="/home/ghost/Documents/My_projects/Business_Projects/Products/Custom-RAG-Lama-ChatBot/Services/Project.pdf"
 
-data= obj.get_sentences()
+    obj= DataExtractor(path)
 
-print(data)
-
-
-
-
-
+    print(obj.get_sentences())
