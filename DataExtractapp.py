51102e6763a6eb2290644ab37279fc1ed41c8dcd
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-12-09 16:01:58 +0530

    basic data processing step integrated

--- DataExtractapp.py
+++ DataExtractapp.py
@@ -1,7 +1,13 @@
-import pdfplumber as pp
-import re
+import pdfplumber as pp # --> handle the pdf file 
+import re #--> handle the regular expression 
+import nltk #---> natural langiuage tool kit for text processing
+from nltk.corpus import stopwords # to rmove sopwords from the image
+from nltk.stem import WordNetLemmatizer
 
+nltk.download("stopwords") #----> rquired internet connection
+nltk.download("wordnet")
 
+lemmatizer = WordNetLemmatizer()
 data=''
 
 with pp.open("Project.pdf") as pdf:
@@ -16,5 +22,16 @@ c_data=[]
 for token in data.split():
      c_data.append(re.sub(r'[^a-zA-z0-9]','',token.lower()))
 
+# print(c_data)
 
-print(c_data)
+
+stop_words = stopwords.words('english')
+
+
+filter_tokens = [w for w in c_data if w not in stop_words]
+
+lemmatized_words = [lemmatizer.lemmatize(w) for w in filter_tokens]
+
+print(lemmatized_words)
+
+
