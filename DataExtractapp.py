e71898351563bab997723534a1d062a7ff9f24fc
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-12-09 15:38:57 +0530

    data tokenizeations is done

--- DataExtractapp.py
+++ DataExtractapp.py
@@ -1,5 +1,20 @@
 import pdfplumber as pp
+import re
+
+
+data=''
 
 with pp.open("Project.pdf") as pdf:
     for page in pdf.pages:
-        print(page.extract_text())
+        data=page.extract_text()
+
+# print(data.split(' '))
+
+c_data=[]
+
+
+for token in data.split():
+     c_data.append(re.sub(r'[^a-zA-z0-9]','',token.lower()))
+
+
+print(c_data)
